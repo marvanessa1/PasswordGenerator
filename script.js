63c42412b934cc7d21c58b1107e263a783352fca
marvanessa1 <marvanessa1@live.com>
2021-11-19 18:39:22 -0800

    added variables for different type of characters and console logged them to make sure they were inputed correctly

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Variables
+
+var NumericCharacters = "0123456789".split(" ");
+var LowecaseCharacters = "abcdefghijklmnopqrstuvwxyz".split(" ");
+var UppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(" ");
+var SpecialCharacters = "`~!@#$%^&*()-=_+[]{}|;',./<>?:".split(" ");
+
+console.log(NumericCharacters);
+console.log(LowecaseCharacters);
+console.log(UppercaseCharacters);
+console.log(SpecialCharacters);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
