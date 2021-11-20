d73a64d484f3e44068a4b5c174ee0ac0fd1c71d2
marvanessa1 <marvanessa1@live.com>
2021-11-19 18:54:09 -0800

    added variables for user input to prompt and confitms.

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ console.log(LowecaseCharacters);
 console.log(UppercaseCharacters);
 console.log(SpecialCharacters);
 
+var NumericInput = prompt("How many characters would you like your password to contain?");
+var LowercaseInput = confirm("Click OK to include lower case characters");
+var UppercaseInput = confirm("Click OK to include upper case characters");
+var SpecialInput = confirm("Click OK to include special characters");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
