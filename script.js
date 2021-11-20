1ab3e1c445929a79e4b7657bd136325438a9f385
marvanessa1 <marvanessa1@live.com>
2021-11-19 23:58:59 -0800

    corrected declared variables, was missing quotation marks in string

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Variables
+//Declared variables
 
-var NumericCharacters = "0123456789".split(" ");
-var LowecaseCharacters = "abcdefghijklmnopqrstuvwxyz".split(" ");
-var UppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(" ");
-var SpecialCharacters = "`~!@#$%^&*()-=_+[]{}|;',./<>?:".split(" ");
+var NumericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+var LowecaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var UppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var SpecialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","=","_","+","[","]","{","}","|",";","'",",",".","/","<",">","?",":"];
 
 console.log(NumericCharacters);
 console.log(LowecaseCharacters);
